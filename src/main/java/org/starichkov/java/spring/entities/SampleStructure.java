361180f87425603f681077222c4f37991291f744
Vadim Starichkov <starichkovva@gmail.com>
2017-05-24 12:42:15 +0300

    Update Spring versions, add Lombok

--- src/main/java/org/starichkov/java/spring/entities/SampleStructure.java
+++ src/main/java/org/starichkov/java/spring/entities/SampleStructure.java
@@ -1,23 +1,20 @@
 package org.starichkov.java.spring.entities;
 
+import lombok.Getter;
+
+import java.security.SecureRandom;
+
 /**
  * @author Vadim Starichkov
  * @since 21.10.2016 16:21
  */
+@Getter
 public class SampleStructure {
     private final String name;
     private final int version;
 
     public SampleStructure() {
         name = "Sample structure";
-        version = 1;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getVersion() {
-        return version;
+        version = new SecureRandom().nextInt();
     }
 }
